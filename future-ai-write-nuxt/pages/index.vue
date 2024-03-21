e6be5ad1a19509db7193befe56a42e16d614d1ff
Max <penghui8411@gmail.com>
2024-03-21 20:21:02 +0800

    release-1.0.0

---
+++ future-ai-write-nuxt/pages/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+// 获取默认配置，并跳转至对应的页面
+const { data } = await httpGet('/write/config/list')
+const defaultConfig = data.find(item => item.isDefault)
+const router = useRouter()
+router.push(`/write/${defaultConfig?.configKey || 'bylw'}`)
+</script>
+
+<template>
+</template>
+
+<style scoped>
+
+</style>
